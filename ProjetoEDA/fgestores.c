6e9a15c173175d081157fc30187bd3bd02b0cc15
hs1lva <a16368@alunos.ipca.pt>
2023-05-04 21:14:58 +0100

    atualizações nas funções e header dos gestores

--- ProjetoEDA/fgestores.c
+++ ProjetoEDA/fgestores.c
@@ -8,14 +8,21 @@
 
 #include "gestores.h"
 
- /**
- @brief Função para fazer login como gestor
- @return 1 Se o login foi bem sucedido
- @return 0 Se ocorreu um erro no login
- */
+#define TAM_NOME_USER 50
+#define TAM_NOME_SENHA 50
+
+ // Definir o user e a senha do gestor
+#define GESTOR_USER "admin"
+#define GESTOR_SENHA "1234"
+
+/**
+@brief Função para fazer login como gestor
+@return 1 Se o login foi bem sucedido
+@return 0 Se ocorreu um erro no login
+*/
 int loginGestor() {
-    char user[MAX_USER];
-    char senha[MAX_SENHA];
+    char user[TAM_NOME_USER];
+    char senha[TAM_NOME_SENHA];
 
     // Solicita o user e a senha do gestor
     printf("User: ");
--- ProjetoEDA/gestores.h
+++ ProjetoEDA/gestores.h
@@ -11,14 +11,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-//Não é necessário nenhuma struct para os gestores
-
-#define MAX_USER 20
-#define MAX_SENHA 20
-
-// Definir o user e a senha do gestor
-#define GESTOR_USER "admin"
-#define GESTOR_SENHA "1234"
+//Não é necessário nenhuma struct para os gestores, apenas dados diretos para entrar como gestor
 
 //Função para fazer login como gestor
 int loginGestor();
